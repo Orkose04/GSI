4b67789ee3f659904c579acab9940a7c6ec8d567
Orkose04 <anjoanina@gmail.com>
2024-02-26 18:20:58 +0300

    Display more

--- GSI/src/composants/listUser.jsx
+++ GSI/src/composants/listUser.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import List from "../listUser/list";
 import { RiRefreshFill } from "react-icons/ri";
 import { useState } from "react";
+import Img from "/img/user1.png";
 const ListUser = () => {
   const [userActif, setUserActif] = useState([]);
   const handleRefresh = () => {
-    fetch("http://192.168.88.27:5000/gsi/users/")
+    fetch("http://10.42.0.1:53/gsi/users/")
       .then((res) => {
         return res.json();
       })
@@ -26,10 +27,27 @@ const ListUser = () => {
           <RiRefreshFill className="iconRefresh" />
         </button>
       </div>
+      <List
+        username={"TestUser"}
+        key={2}
+        img={Img}
+        connectedTime={"5 seconds"}
+      />
+      <List
+        username={"TestUser2"}
+        key={2}
+        img={Img}
+        connectedTime={"50 minutes"}
+      />
       {userActif.map((user) => (
         <>
           <br />
-          <List username={user.nom} key={user.id} img={user.img} />
+          <List
+            username={user.nom}
+            key={user.id}
+            img={user.img}
+            connectedTime={1}
+          />
           <br />
           <hr />
         </>
--- GSI/src/listUser/list.jsx
+++ GSI/src/listUser/list.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { GoDotFill } from "react-icons/go";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import "./list.css";
 
-export const List = ({ username, img }) => {
+export const List = ({ username, img, connectedTime }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const showMore = useCallback(() => {
+    setShowDetails((prevShowDetails) => !prevShowDetails);
+  }, []);
+
   return (
-    <div className="bigContainer container-items">
-      <span className="pdp">
-        <img src={img} alt="user1" className="pdp" />
-      </span>
-      <div className="user">
-        <div className="user-info">
-          <p className="username">{username}</p>
-          <p className="connected">
-            <GoDotFill className="green-dot" />
-            Connected
-          </p>
+    <>
+      <div className="userCard">
+        <div className="bigContainer container-items">
+          <div className="user">
+            <div className="pdp">
+              <img src={img} alt="user1" className="pdp" />
+            </div>
+            <div className="right">
+              <div className="user-info">
+                <p className="username">{username}</p>
+                <p className="connected">
+                  <GoDotFill className="green-dot" />
+                  Connected
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        <button className="show-more">
+          <BsThreeDotsVertical className="show-more-dots" onClick={showMore} />
+        </button>
       </div>
-    </div>
+      {showDetails && (
+        <div className="details">
+          {/* Add the additional information you want to display here */}
+          <ul>
+            <li>Connected since: {connectedTime}</li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
